2ff2971e18292a32ad3dd65bdec154d4f64f54fa
beflylui <33077555+beflylui@users.noreply.github.com>
2019-06-15 21:05:40 +0900

    Create lecture.php

---
+++ web/teacher/lecture.php
@@ -0,0 +1,226 @@
+<?php session_start();
+	include 'db.php';
+	$link = connect_db($host,$dbid,$dbpw,$dbname);
+	mysqli_set_charset($link,"utf8");
+?>
+<!DOCTYPE HTML>
+
+<html lang="ko">
+	<head>
+		<title>VOICE YOU</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="../assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header" class="skel-layers-fixed">
+				<h1><a href="../index.php">VOICE YOU</a></h1>
+				<?php
+					if(isset($_SESSION["id"])) {
+						//include 'db.php';
+						//echo "<script>alert('smooth1')</script>";
+						//$link = connect_db($host, $dbid, $dbpw, $dbname);
+						$getid= $_SESSION['id'];
+						//mysqli_set_charset($link, "utf8");
+						$select_query = "select ID, Seen from User_comment where user_id='$getid'"; 
+						$result_set = mysqli_query($link, $select_query);
+						$select_queryt = "select teacher, pass from User where ID='$getid'";
+						$result_sett = mysqli_query($link, $select_queryt);	
+
+		
+						$flag = 1;
+						$count = 0;
+
+						$rowt = mysqli_fetch_assoc($result_sett);
+
+						while($row = mysqli_fetch_assoc($result_set)){
+							//echo "<script>alert('smooth1')</script>";
+							if($row["Seen"]==$flag and!($getid == $row["ID"])){
+								$count++;
+							}
+						}
+						if($count == 0){
+				?>
+				<a  href="#nav">Menu</a>
+				<?php
+						}else {
+				?>
+				<a style="color:red;" href="#nav">Menu</a>
+				<?php
+						}
+					}else {
+				?>
+				<a  href="#nav">Menu</a>
+				<?php
+					}
+				?>
+			</header>
+
+		<!-- Nav(메뉴) -->
+		<?php if(!isset($_SESSION["id"])) { ?> <!--로그인 x-->
+			<nav id="nav">
+				<ul class="links">
+					<li><a href="../index.php">Home</a></li>
+					<li><a href="../login/login.html">Login</a></li>
+					<li><a href="../join/join.php">Join</a></li>
+					<li><a href="../upload/upload.php">Upload</a></li>
+					<li><a href="../chart/chart.php">Chart</a></li>
+				</ul>
+			</nav>
+			<?php }else { ?> <!--로그인 o-->
+			<nav id="nav">
+				<ul class="links">
+
+					<?php 
+						if($rowt["teacher"]==$flag) { //선생님
+					?>
+					<li><a href="#" style="text-transform: none;"><img src="../images/person.jpg" style="width: auto; height:20px;">&nbsp;<?php echo $_SESSION["wh"];?>&nbsp;teacher</a></li>
+					<?php
+						}else if($rowt["pass"]==$flag){ //사용자
+					?>
+					<li><a href="#" style="text-transform: none;"><img src="../images/person.jpg" style="width: auto; height:20px;">&nbsp;<?php echo $_SESSION["wh"];?>&nbsp;student</a></li>
+					<?php
+						}else {
+					?>
+					<li><a href="#" style="text-transform: none;"><img src="../images/person.jpg" style="width: auto; height:20px;">&nbsp;<?php echo $_SESSION["wh"];?>&nbsp;</a></li>
+					<?php
+						}
+					?>
+
+					<li><a href="../index.php">Home</a></li>
+					<li><a href="../logout/logout.php">Logout</a></li>
+					<li><a href="../upload/upload.php">Upload</a></li>
+					<li><a href="../chart/chart.php">Chart</a></li>
+
+					<?php 
+						if($rowt["teacher"]==$flag) { //선생님
+					?>
+					<li><a href="#">Teacher page</a>
+						<ul>
+							<li><a href="../teacher/upload.php">upload</a></li>
+							<li><a href="../teacher/dashboard.php">Dashboard</a></li>
+							<li><a href="../teacher/edit_text.php">Edit text</a></li>
+						</ul>
+					</li>
+					<?php
+						}
+					?>
+
+					<?php
+						if($count == 0) {
+					?>
+					<li><a href="#"><img src="../images/notification1.png" alt="알림" style="width: auto; height: 1.25em; vertical-align:middle;" >&nbsp;Notification</a><li>
+					<?php
+						}else {
+					?>
+					<li><a href="#"><?php echo $count; ?><img src="../images/notification2.png" alt="알림" style="width: auto; height: 1.25em; vertical-align:middle;" >&nbsp;Notification</a><li>
+					<?php
+						}
+					?>
+				</ul>
+			</nav>
+			<?php } ?>
+
+		<!-- Main -->
+			<section id="main" class="wrapper">
+				<div class="container" style ="width:900px;">
+					<header class="major special">
+						<h2>LECTURE</h2>
+						
+					</header>
+					<hr>
+					
+					<?php
+						$http_host = $_SERVER['HTTP_HOST'];
+						$request_uri = $_SERVER['REQUEST_URI'];
+						$url = 'http://' . $http_host . $request_uri;
+						$strTok = explode('?', $url);
+
+						$category = $strTok[1];
+						$id = $strTok[2];
+						$date = $strTok[3];
+
+						$select_query1 = "SELECT * FROM Lecture where id='$id' and category='$category' and date='$date'"; 
+						$result_set1 = mysqli_query($link, $select_query1);
+
+						$row1 = mysqli_fetch_array($result_set1);
+					?>
+
+					<div class="example" style="padding: 6% 5% 5%; border: 1px solid #ccc; text-align:center;">
+						<?php echo $row1['title']; ?>
+					
+						<br>
+						<video controls preload="auto" width="650px" height="500px" data-setup="{}">
+							<source src="<?php echo $row1['video_file_address']; ?>" type='video/mp4'>
+						</video>
+						<br>
+						<br>
+						<?php echo $row1['content_text']; ?>
+					</div>
+
+					<hr>
+										
+
+					
+
+				</div>
+			</section>
+
+		<!-- Footer -->
+		<footer id="footer">
+				<div class="inner">
+					<ul>
+						<div>
+							<a href="#" style="text-decoration:none">이용약관</a> | <a href="#" style="text-decoration:none">개인정보처리방침</a>
+						</div>
+					</ul>
+
+					<ul class="icons">
+						<li><a href="#" class="icon fa-facebook">
+							<span class="label">Facebook</span>
+						</a></li>
+						<li><a href="#" class="icon fa-twitter">
+							<span class="label">Twitter</span>
+						</a></li>
+						<li><a href="#" class="icon fa-instagram">
+							<span class="label">Instagram</span>
+						</a></li>
+						<li><a href="#" class="icon fa-linkedin">
+							<span class="label">LinkedIn</span>
+						</a></li>
+					</ul>
+					<ul>
+						<span>경북대학교</span><br>
+						<span>2019 1학기 소프트웨어 공학</span><br>
+						<span>교수님 : 정설영</span><br>
+						<span>팀원 : 박건우, 이은지, 이주형, 리카이신</span> &nbsp <span>15조</span><br>
+						
+						
+					</ul>
+
+					<ul class="copyright">
+						<li>Copyright &copy; 2019 Voiceyou, All rights reserved.</li>
+						<!--
+						<li>Images: <a href="http://unsplash.com">Unsplash</a>.</li>
+						<li>Design: <a href="http://templated.co">TEMPLATED</a>.</li>
+						-->
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="../assets/js/jquery.min.js"></script>
+			<script src="../assets/js/skel.min.js"></script>
+			<script src="../assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="../assets/js/main.js"></script>
+
+		
+
+	</body>
+</html>
